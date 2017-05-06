9036eba7b4aa06927c43924b1abb1fe369620b3b
Alexey Sergienko <lexsergienko@gmail.com>
2017-05-06 18:31:09 +0300

    homework1 done

--- src/index.js
+++ src/index.js
@@ -6,7 +6,11 @@
  Функция должна принимать один аргумент и возвращать его
  */
 function returnFirstArgument(arg) {
-}
+	return arg;
+};
+
+var result = returnFirstArgument(Math.floor(Math.random()*(100-0)));
+console.log('Exercise 1: '+result);
 
 /*
  Задание 2:
@@ -14,8 +18,12 @@ function returnFirstArgument(arg) {
  Функция должна принимать два аргумента и возвращать сумму переданных значений
  Значение по умолчанию второго аргумента должно быть 100
  */
-function defaultParameterValue(a, b) {
-}
+function defaultParameterValue(a, b = 100) {
+	return a + b;
+};
+
+var sum = defaultParameterValue(50, 30);
+console.log('Excercise 2: '+sum);
 
 /*
  Задание 3:
@@ -24,7 +32,15 @@ function defaultParameterValue(a, b) {
  Количество переданных аргументов заранее неизвестно
  */
 function returnArgumentsArray() {
-}
+	var arr = [];
+	for (var i = 0, len = arguments.length; i < len; i++) {
+		arr.push(arguments[i]);
+	}
+	return arr;
+};
+
+console.log('Excercise 3: '+returnArgumentsArray('1', 2, 'three'));
+
 
 /*
  Задание 4:
@@ -32,7 +48,14 @@ function returnArgumentsArray() {
  Функция должна принимать другую функцию и возвращать результат вызова переданной функции
  */
 function returnFnResult(fn) {
-}
+	return 'random number is ' + fn();
+};
+
+function F() {
+	return Math.floor(Math.random()*(100-0));
+};
+
+console.log('Excercise 4: '+returnFnResult(F));
 
 /*
  Задание 5:
@@ -40,8 +63,14 @@ function returnFnResult(fn) {
  Функция должна принимать число (значение по умолчанию - 0) и возвращать функцию (F)
  При вызове F, переданное число должно быть увеличено на единицу и возвращено из F
  */
-function returnCounter(number) {
+function returnCounter(number = 0) {
+	function F() {
+		return ++number;
+	}
+	return F();
 }
+
+console.log('Excercise 5: '+returnCounter());
 
 /*
  Задание 6 *:
@@ -49,8 +78,15 @@ function returnCounter(number) {
  Функция должна принимать другую функцию (F) и некоторое количество дополнительных аргументов
  Функция должна привязать переданные аргументы к функции F и вернуть получившуюся функцию
  */
-function bindFunction(fn) {
+function bindFunction(fn, x, n) {
+	return fn(x, n);
 }
+
+function pow(x, n) {
+	return x ** n;
+}
+
+console.log('Excercise 6: '+bindFunction(pow, 2, 2));
 
 export {
     returnFirstArgument,
